116ad10bee5f21f21258c46ac664ed821f22655e
Mandeep Singh Baines <msb@chromium.org>
2011-04-27 15:16:37 -0700

    pre-upload.py: Generalize a framework and add kernel checkpatch.pl
    pre-upload.py is now capable of checking kernel committs.
    
    Change-Id: I6d055d384310f6813d3a8c23e4bfe30ead09bf15

--- pre-upload.py
+++ pre-upload.py
@@ -1,13 +1,62 @@
-import random
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+
+# Helpers
+
+def _get_hooks_dir():
+  """Returns the absolute path to the repohooks directory"""
+  cmd = ['repo', 'forall', 'chromiumos/repohooks', '-c', 'pwd']
+  return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].strip()
+
+def _get_diff():
+  """Returns the diff for this project"""
+
+  # TODO(msb) iterate over each commit
+  cmd = ['git', 'show', 'HEAD']
+  return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+
+# Hooks
+
+def _run_checkpatch(project):
+  """Runs checkpatch.pl on the given project"""
+  hooks_dir = _get_hooks_dir()
+  cmd = ['%s/checkpatch.pl' % hooks_dir, '-']
+  p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+  output = p.communicate(_get_diff())[0]
+  if p.returncode:
+    raise Exception('checkpatch.pl errors/warnings\n\n' + output)
+
+# Base
+
+def _setup_project_hooks():
+  """Returns a dictionay of callbacks: dict[project] = [callback1, callback2]"""
+  return {
+    "chromiumos/third_party/kernel": [_run_checkpatch]
+    }
+
+def _run_project_hooks(project, hooks):
+  """For each project run its project specific hook from the hooks dictionary"""
+  cmd = ['repo', 'forall', project, '-c', 'pwd']
+  proj_dir = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+  proj_dir = proj_dir.strip()
+  if project in hooks:
+    pwd = os.getcwd()
+    # hooks assume they are run from the root of the project
+    os.chdir(proj_dir)
+    for hook in hooks[project]:
+        hook(project)
+    os.chdir(pwd)
+
+# Main
 
 def main(project_list, **kwargs):
-  print ('These projects will be uploaded: %s' %
-         ', '.join(project_list))
-  print ('I am being a good boy and ignoring anything in kwargs\n'
-         'that I don\'t understand.')
-  print 'I fail 50% of the time.  How flaky.'
-  if random.random() <= .5:
-    raise Exception('Pre-upload hook failed.  Have a nice day.')
+  hooks = _setup_project_hooks()
+  for project in project_list:
+    _run_project_hooks(project, hooks)
 
 if __name__ == '__main__':
   main()

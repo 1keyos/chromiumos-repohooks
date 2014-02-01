65d93c1ed3a6143b9711f9caa3caeb289f6d8664
Mike Frysinger <vapier@chromium.org>
2014-02-01 02:59:46 -0500

    leverage chromite cros_test_lib module
    Use the helper modules chromite already provides to cover common testcase
    issues like test setup/teardown.
    
    BUG=chromium:340036
    TEST=`./pre-upload_unittest.py` still works
    
    Change-Id: I9aaed2b39536c17a2b2a16ae39aa5f7c32b0bc88
    Reviewed-on: https://chromium-review.googlesource.com/184621
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- pre-upload_unittest.py
+++ pre-upload_unittest.py
@@ -11,17 +11,23 @@ from __future__ import print_function
 import mox
 import os
 import sys
-import unittest
-
 
 # pylint: disable=W0212
-if __name__ == '__main__':
+# We access private members of the pre_upload module all over the place.
+
+# If repo imports us, the __name__ will be __builtin__, and the wrapper will
+# be in $CHROMEOS_CHECKOUT/.repo/repo/main.py, so we need to go two directories
+# up. The same logic also happens to work if we're executed directly.
+if __name__ in ('__builtin__', '__main__'):
   sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '..', '..'))
+
+from chromite.lib import cros_test_lib
+
 
 pre_upload = __import__('pre-upload')
 
 
-class TryUTF8DecodeTest(unittest.TestCase):
+class TryUTF8DecodeTest(cros_test_lib.TestCase):
   """Verify we sanely handle unicode content."""
 
   def runTest(self):
@@ -32,21 +38,16 @@ class TryUTF8DecodeTest(unittest.TestCase):
     self.assertEquals('\x80', pre_upload._try_utf8_decode('\x80'))
 
 
-class CheckNoLongLinesTest(unittest.TestCase):
+class CheckNoLongLinesTest(cros_test_lib.MoxTestCase):
   """Tests for _check_no_long_lines."""
 
   def setUp(self):
-    self.mocker = mox.Mox()
-    self.mocker.StubOutWithMock(pre_upload, '_filter_files')
-    self.mocker.StubOutWithMock(pre_upload, '_get_affected_files')
-    self.mocker.StubOutWithMock(pre_upload, '_get_file_diff')
+    self.mox.StubOutWithMock(pre_upload, '_filter_files')
+    self.mox.StubOutWithMock(pre_upload, '_get_affected_files')
+    self.mox.StubOutWithMock(pre_upload, '_get_file_diff')
     pre_upload._get_affected_files(mox.IgnoreArg()).AndReturn(['x.py'])
     pre_upload._filter_files(
         ['x.py'], mox.IgnoreArg(), mox.IgnoreArg()).AndReturn(['x.py'])
-
-  def tearDown(self):
-    self.mocker.UnsetStubs()
-    self.mocker.VerifyAll()
 
   def runTest(self):
     pre_upload._get_file_diff(mox.IgnoreArg(), mox.IgnoreArg()).AndReturn(
@@ -59,7 +60,7 @@ class CheckNoLongLinesTest(unittest.TestCase):
          (7, u"#  define " + (u"x" * 80)),    # OK (compiler directive)
          (8, u"#define" + (u"x" * 74)),       # Too long
          ])
-    self.mocker.ReplayAll()
+    self.mox.ReplayAll()
     failure = pre_upload._check_no_long_lines('PROJECT', 'COMMIT')
     self.assertTrue(failure)
     self.assertEquals('Found lines longer than 80 characters (first 5 shown):',
@@ -69,47 +70,41 @@ class CheckNoLongLinesTest(unittest.TestCase):
                       failure.items)
 
 
-class CheckKernelConfig(unittest.TestCase):
+class CheckKernelConfig(cros_test_lib.MoxTestCase):
   """Tests for _kernel_configcheck."""
-
-  def setUp(self):
-    self.mocker = mox.Mox()
-
-  def tearDown(self):
-    self.mocker.UnsetStubs()
 
   def runTest(self):
     # Mixed changes, should fail
-    self.mocker.StubOutWithMock(pre_upload, '_get_affected_files')
+    self.mox.StubOutWithMock(pre_upload, '_get_affected_files')
     pre_upload._get_affected_files(mox.IgnoreArg()).AndReturn(
         ['/kernel/files/chromeos/config/base.config',
          '/kernel/files/arch/arm/mach-exynos/mach-exynos5-dt.c'
         ])
-    self.mocker.ReplayAll()
+    self.mox.ReplayAll()
     failure = pre_upload._kernel_configcheck('PROJECT', 'COMMIT')
     self.assertTrue(failure)
 
     # Code-only changes, should pass
-    self.mocker.UnsetStubs()
-    self.mocker.StubOutWithMock(pre_upload, '_get_affected_files')
+    self.mox.UnsetStubs()
+    self.mox.StubOutWithMock(pre_upload, '_get_affected_files')
     pre_upload._get_affected_files(mox.IgnoreArg()).AndReturn(
         ['/kernel/files/Makefile',
          '/kernel/files/arch/arm/mach-exynos/mach-exynos5-dt.c'
         ])
-    self.mocker.ReplayAll()
+    self.mox.ReplayAll()
     failure = pre_upload._kernel_configcheck('PROJECT', 'COMMIT')
     self.assertFalse(failure)
 
     # Config-only changes, should pass
-    self.mocker.UnsetStubs()
-    self.mocker.StubOutWithMock(pre_upload, '_get_affected_files')
+    self.mox.UnsetStubs()
+    self.mox.StubOutWithMock(pre_upload, '_get_affected_files')
     pre_upload._get_affected_files(mox.IgnoreArg()).AndReturn(
         ['/kernel/files/chromeos/config/base.config',
         ])
-    self.mocker.ReplayAll()
+    self.mox.ReplayAll()
     failure = pre_upload._kernel_configcheck('PROJECT', 'COMMIT')
     self.assertFalse(failure)
 
 
 if __name__ == '__main__':
-  unittest.main()
+  cros_test_lib.main()

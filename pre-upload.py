4844f8964c596dbdb93507b20511d5cb427c7fad
Daisuke Nojiri <dnojiri@chromium.org>
2015-10-08 09:54:33 -0700

    pre-upload: allow change-id
    This change makes checkpatch ignore change-id check, which prevents CLs from
    being uploaded to Gerrit.
    
    BUG=none
    BRANCH=master
    TEST=repo upload this CL without the error
    
    Change-Id: I02cc045b73e3aad98e17c2b95c8e6d267a456dbe
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/304628
    Reviewed-by: Filipe Brandenburger <filbranden@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- pre-upload.py
+++ pre-upload.py
@@ -1098,6 +1098,7 @@ def _run_checkpatch(_project, commit, options=()):
   # information on that file in our source trees, so let's suppress the
   # warning.
   options.append('--ignore=FILE_PATH_CHANGES')
+  options.append('--ignore=GERRIT_CHANGE_ID')
   cmd = ['%s/checkpatch.pl' % hooks_dir] + options + ['-']
   cmd_result = cros_build_lib.RunCommand(cmd=cmd,
                                          print_cmd=False,

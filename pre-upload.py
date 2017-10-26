c97dab173df1fb8f0289289fc32f71efef828abb
Mike Frysinger <vapier@chromium.org>
2017-10-26 04:33:34 -0400

    pre-upload: move bluez tab check to bluez repo
    BUG=chromium:466264
    TEST=None
    CQ-DEPEND=CL:738358
    
    Change-Id: I5df3f39b04b6231c4451f352966f17f9dc3df937
    Reviewed-on: https://chromium-review.googlesource.com/738359
    Commit-Ready: Miao-chen Chou <mcchou@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>

--- pre-upload.py
+++ pre-upload.py
@@ -488,7 +488,6 @@ def _check_no_tabs(_project, commit, options=()):
   """Checks there are no unexpanded tabs."""
   TAB_OK_PATHS = [
       r"/src/third_party/u-boot/",
-      r"/src/third_party/bluez/",
       r".*\.ebuild$",
       r".*\.eclass$",
       r".*/[M|m]akefile$",
